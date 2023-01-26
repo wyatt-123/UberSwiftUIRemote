1d036c5ef6c78b8d0647f19685d68541a1e15f98
wyatt <f227006900@hmail.com>
2023-01-26 11:36:18 +0800

    Initial Commit

---
+++ UberSwiftUI/UberSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUIApp.swift
+//  UberSwiftUI
+//
+//  Created by 林家麒 on 2023/1/26.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
